c4f8a6e09f85ae5ce601f005e3dd1747625a1318
= <connor@connorgiles.com>
2022-09-28 02:05:24 -0400

    Added comments

--- client.py
+++ client.py
@@ -1,8 +1,16 @@
+'''
+Each menu type will extend MenuClient
+'''
+
+
 class MenuClient:
+
     def get_menu_page(self, page):
+        # Each client type must implement this method
         raise NotImplementedError
 
     def get_menu(self,):
+        # Gets full menu - paginating through all pages
         data, total_pages = self.get_menu_page(0)
         for page in range(1, total_pages):
             data.extend(self.get_menu_page(page)[0])
